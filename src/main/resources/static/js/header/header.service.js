9386906a14616af6e46a39e5fda94b8317a75813
Michał Szostak <michal.szostak@cyfronet.pl>
2017-06-05 14:36:07 +0200

    fixed bug in js template path

--- src/main/resources/static/js/header/header.service.js
+++ src/main/resources/static/js/header/header.service.js
@@ -1,6 +1,6 @@
 angular.module('indigo')
     .component('indigoHeader', {
-        templateUrl: 'js/components/header/header.view.html',
+        templateUrl: 'js/header/header.view.html',
         controller: function ($scope, $route, SESSION) {
             this.$route = $route
             if (SESSION.roles.indexOf("ROLE_PROVIDER") != -1)
